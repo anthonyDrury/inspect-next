fd8e5c9b9536d09285f4d1e4ca6d1d281f130c53
anthonyDrury <anthony.drury1@gmail.com>
2020-04-14 07:37:59 +1000

    feat: add state to local storage

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -7,20 +7,35 @@ import thunk from "redux-thunk";
 import { initialState } from "../../common/constant";
 import { State } from "../../types/redux.types";
 import reducer from "../reducers/reducer";
+import { isDefined } from "../../common/support";
 
 const composeEnhancers: typeof compose =
   // tslint:disable-next-line: no-string-literal
   ((window as any)["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] as typeof compose) ||
   compose;
 
+//gets starting state from localStorage if present,
+// otherwise sets as default initial state
+function getStartState(): State {
+  const storageItem: string | null = localStorage.getItem("inspectNextState");
+
+  return isDefined(storageItem)
+    ? (JSON.parse(storageItem!) as State)
+    : initialState;
+}
+
 function configureStore(): Store {
   return createStore(
     reducer,
-    initialState,
+    getStartState(),
     composeEnhancers(applyMiddleware(thunk))
   );
 }
 
 const store: Store<State, any> = configureStore();
+
+store.subscribe((): void => {
+  localStorage.setItem("inspectNextState", JSON.stringify(store.getState()));
+});
 
 export default store;
