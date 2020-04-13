fa760cdf9b78b9db3edfc37c7d7491d369dec319
anthonyDrury <anthony.drury1@gmail.com>
2020-04-14 07:53:13 +1000

    fix: state and style bugs

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -8,20 +8,26 @@ import { initialState } from "../../common/constant";
 import { State } from "../../types/redux.types";
 import reducer from "../reducers/reducer";
 import { isDefined } from "../../common/support";
+import { mapListToWeatherMap } from "../../common/weather.support";
 
 const composeEnhancers: typeof compose =
   // tslint:disable-next-line: no-string-literal
   ((window as any)["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] as typeof compose) ||
   compose;
 
-//gets starting state from localStorage if present,
+// gets starting state from localStorage if present,
 // otherwise sets as default initial state
 function getStartState(): State {
   const storageItem: string | null = localStorage.getItem("inspectNextState");
+  const state: State = JSON.parse(storageItem!) as State;
 
-  return isDefined(storageItem)
-    ? (JSON.parse(storageItem!) as State)
-    : initialState;
+  // need to remap the forecast since Map objects do not parse through JSON
+  if (state.fiveDay?.forecast !== undefined) {
+    state.fiveDay.mappedForecast = mapListToWeatherMap(
+      state.fiveDay?.forecast.list
+    );
+  }
+  return isDefined(storageItem) ? state : initialState;
 }
 
 function configureStore(): Store {
